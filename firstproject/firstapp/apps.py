c1d1572c9189ce6132fdfc00e2ef8fabc64cc1b1
TheSolobaid <thesolobaid@gmail.com>
2023-05-16 10:40:08 +0200

    tets

--- firstproject/firstapp/apps.py
+++ firstproject/firstapp/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class MyfirstappConfig(AppConfig):
+class FirstappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myfirstapp'
+    name = 'firstapp'
+
